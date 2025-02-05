01f6cf65e159c20fedaf0398df726361f94952c9
mrsvolodya <vovamursky@gmail.com>
2025-02-05 22:18:27 +0200

    fetch data

--- src/components/Main/ProductRow.tsx
+++ src/components/Main/ProductRow.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
 const ProductRow = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default ProductRow
+export default ProductRow;
