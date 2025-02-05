398346162a9d27f4fa18681aeb896fb6bb24cc22
mrsvolodya <vovamursky@gmail.com>
2025-02-05 14:49:53 +0200

    initial commit add fonts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-header h-20">
+      <div className=""><p>Company <span>Logo</span></p></div>
+    </header>
+  )
+}
+
+export default Header;
